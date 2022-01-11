2df2296bde3f576e68667e744e9d87cb2eb58196
Osama <95348282+OsamaRashid585@users.noreply.github.com>
2022-01-11 02:51:42 -0800

    Update Find-prime-number.cpp

--- Find-prime-number.cpp
+++ Find-prime-number.cpp
@@ -2,12 +2,18 @@
 using namespace std;
 
 int main(){
+    int div = 2;
     int num[] = {12,7,6,3};
     for (int i = 0; i < 4; i++)
     {
-        if(num[i] % 2 != 0){
+        while(div < num[i])
+        {
+            if(num[i] % div != 0){
             cout << num[i] << endl;
+        }
+           div++;
         }
     }
     
 }
+
