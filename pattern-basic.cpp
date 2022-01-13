bd9b84759aec65558eeada3021b176c4a6c3e54e
Osama <95348282+OsamaRashid585@users.noreply.github.com>
2022-01-13 07:29:13 -0800

    pattern-basic

---
+++ pattern-basic.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  // int r = 5;
+  // int c = 4;
+
+  // for(int i = 1; i <= r; i++){
+  //   for(int j = 1; j <= c; j++){
+  //     cout << " *";
+  //   }
+  //   cout << endl;
+  // }
+
+  /********2nd*****/
+
+  // int r = 5;
+
+  // for(int i = 1; i <= r; i++){
+  //   for(int j = 1; j <= i; j++){
+  //     cout << " *";
+  //   }
+  //   cout << endl;
+  // }
+
+////*********3rd****//////
+  // int r = 5;
+
+  // for(int i = 1; i <= r; i++){
+  //   for(int j = i; j <= r; j++){
+  //     cout << " *";
+  //   }
+  //   cout << endl;
+  // }
+}
