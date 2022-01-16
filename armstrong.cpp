13aa25c20261353722ac554f1c0407f357c14274
Osama <95348282+OsamaRashid585@users.noreply.github.com>
2022-01-16 08:28:43 -0800

    check num is armstrong

---
+++ armstrong.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 153;
+    int a;
+    int b;
+    int c;
+
+    a = n % 10;
+    b = (n/10) % 10;
+    c = (n/100) % 10;
+
+    a = a*a*a;
+    b = b*b*b;
+    c = c*c*c;
+
+    int arms = a + b + c;
+    
+    if(arms = n){
+        cout << "number is armstrom";
+    }
+    else{
+        cout << "not armstorme";
+    }
+}
