aba2589641e11b6cf89ed5450fcc6b631844cf72
Osama <95348282+OsamaRashid585@users.noreply.github.com>
2022-01-30 01:52:13 -0800

    Update find-sum-of-n.cpp

--- find-sum-of-n.cpp
+++ find-sum-of-n.cpp
@@ -1,21 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int n = 10;
-    int sum = 0;
-
-    for(int i = 1; i <= n; i++){
+int sumofn( int n){
+    int sum;
+    for (int i = 0; i <= n; i++)
+    {
         sum = sum + i;
-
-        if(i == n){
-            cout << sum;
-        }
     }
+    return sum;
+    
 }
-
-// step-1 run loop for p-1 because first run muliply 2num (2*2)
-// step-2 store final nth number in varibale;
-// step-3 equal the nth number to n; // 4
-// step-4 make oldn var to store inp n and use on loop n;
-// step-5 check condition (is it final loop call) then print nth
+int main(){
+    cout << sumofn(5);
+}
