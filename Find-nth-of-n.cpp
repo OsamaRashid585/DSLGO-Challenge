b007a92e57303bdbb8071c8917676f36b465c90c
Osama <95348282+OsamaRashid585@users.noreply.github.com>
2022-03-05 01:24:51 -0800

    Update Find-nth-of-n.cpp

--- Find-nth-of-n.cpp
+++ Find-nth-of-n.cpp
@@ -1,26 +1,17 @@
 #include <iostream>
 using namespace std;
 
-void NthSys(int n,int p){
-    int oldn = n;
-    for (int i = 0; i < (p-1); i++)
+int Nth(int num, int power){
+    int old = num;
+    int ans;
+    for (int i = 2; i <= power; i++)
     {
-        int nth = n * oldn;
-        n = nth;
-
-        if(i == (p-2)){
-            cout << nth << endl;
-        }
+       ans = (num *= old);
     }
-        
+    return ans;
 }
 
-int main(){
-    NthSys(2,4);
+int main()
+{
+    cout << Nth(2,4);
 }
-
-// step-1 run loop for p-1 because first run muliply 2num (2*2)
-// step-2 store final nth number in varibale;
-// step-3 equal the nth number to n; // 4
-// step-4 make oldn var to store inp n and use on loop n;
-// step-5 check condition (is it final loop call) then print nth
